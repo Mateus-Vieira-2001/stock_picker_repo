3efd44c68f52ae55cb3bd9caa346a3e5dd22b309
Mateus Vieira <mateus.vieira@sidedoor.com.br>
2024-01-25 15:01:36 -0300

    t

--- main.rb
+++ main.rb
@@ -1,14 +1,27 @@
+
+
 def stock_picker(stock)
   profit = 0
+  profit_sub = 0
+  buy_array =[]
   stock.each_with_index do |item,endereco|
     stock.each_with_index do |item2,endereco2|
       if item<item2 && endereco<endereco2
-        buy_array << item
+        profit_sub= item2-item
+        #puts profit_sub
+
+
+        if item2-item>profit
+          profit = profit_sub
+          buy_array =[endereco,endereco2]
+        end
       end
     end
-    buying_price = buy_array.min
+
+
   end
+  return buy_array
 end
 
 stock = [17,3,6,9,15,8,6,1,10]
-stock_picker(stock)
+puts stock_picker(stock)
